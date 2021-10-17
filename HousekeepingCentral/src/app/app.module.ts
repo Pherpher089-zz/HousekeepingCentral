1ebe3285b7dcc1c3a17d1491c7f8eba5bf42b0dd
miguel diaz <diaz4674@gmail.com>
2021-10-17 05:37:30 -0700

    hotel rates api working

--- HousekeepingCentral/src/app/app.module.ts
+++ HousekeepingCentral/src/app/app.module.ts
@@ -3,23 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { BoardDisplayComponent } from './board-display/board-display.component';
+import { HotelRatesComponent } from './hotel-rates/hotel-rates.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    BoardDisplayComponent
-  ],
+  declarations: [AppComponent, BoardDisplayComponent, HotelRatesComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    HttpClientModule,
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
